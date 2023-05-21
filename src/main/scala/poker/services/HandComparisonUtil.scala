b24ccd9953d4ae8f73db5d6b05a3aae03763c656
Kirill Topchy <kirilltopchy@gmail.com>
2023-05-21 14:02:05 +0300

    Hand comparison logic added.

--- src/main/scala/poker/services/HandComparisonUtil.scala
+++ src/main/scala/poker/services/HandComparisonUtil.scala
@@ -1,14 +1,69 @@
 package poker.services
 
 import poker.domain.game.Outcome
-import poker.domain.player.Hand
+import poker.domain.player.{Hand, Pair}
 
 object HandComparisonUtil {
-
-  // TODO: add hand evaluation logic.
-  def compare(player: Hand, dealer: Hand): Outcome =
-    if (player == dealer)
-      Outcome.Tied
-    else
+  def compare(playerHand: Hand, dealerHand: Hand): Outcome = {
+    val playerHandRank = playerHand.getRank
+    val dealerHandRank = dealerHand.getRank
+    if (playerHandRank.handValue > dealerHandRank.handValue)
       Outcome.PlayerWon
+    else if (playerHandRank.handValue < dealerHandRank.handValue)
+      Outcome.DealerWon
+    else
+      compareHandsWithEqualRanks(playerHand, dealerHand)
+  }
+
+  private def compareHandsWithEqualRanks(playerHand: Hand, dealerHand: Hand): Outcome =
+    playerHand.getRank match {
+      case Pair => comparePairHands(playerHand, dealerHand)
+      case _    => compareHandsByHighestCard(playerHand, dealerHand)
+    }
+
+  private def compareHandsByHighestCard(playerHand: Hand, dealerHand: Hand): Outcome = {
+    val playerHighestCardValue = handHighestCardValue(playerHand)
+    val dealerHighestCardValue = handHighestCardValue(dealerHand)
+
+    if (playerHighestCardValue > dealerHighestCardValue)
+      Outcome.PlayerWon
+    else if (playerHighestCardValue < dealerHighestCardValue)
+      Outcome.DealerWon
+    else
+      Outcome.Tied
+  }
+
+  private def comparePairHands(playerHand: Hand, dealerHand: Hand): Outcome = {
+    val playerPairRank = pairHandPairCardsRankValue(playerHand)
+    val dealerPairRank = pairHandPairCardsRankValue(dealerHand)
+
+    if (playerPairRank > dealerPairRank)
+      Outcome.PlayerWon
+    else if (playerPairRank < dealerPairRank)
+      Outcome.DealerWon
+    else {
+      val playerThirdCardRank = pairHandThirdCardRankValue(playerHand, playerPairRank)
+      val dealerThirdCardRank = pairHandThirdCardRankValue(dealerHand, playerPairRank)
+
+      if (playerThirdCardRank > dealerThirdCardRank)
+        Outcome.PlayerWon
+      else if (playerThirdCardRank < dealerThirdCardRank)
+        Outcome.DealerWon
+      else
+        Outcome.Tied
+    }
+  }
+
+  private def handHighestCardValue(hand: Hand): Int = hand.cards.map(_.rank.value).max
+
+  private def pairHandPairCardsRankValue(hand: Hand): Int =
+    hand.cards
+      .groupBy(_.rank)
+      .collect {
+        case (rank, cards) if cards.length == 2 => rank.value
+      }
+      .max
+
+  private def pairHandThirdCardRankValue(hand: Hand, pairRankValue: Int): Int =
+    hand.cards.filterNot(_.rank.value == pairRankValue).head.rank.value
 }
